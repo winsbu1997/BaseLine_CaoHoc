29e29c1edbed4fbf2e62df449e2ab247ebc4a857
winsbu1997 <38660429+winsbu1997@users.noreply.github.com>
2024-08-20 15:46:19 +0700

    update code 20-8

--- utils/data_preprocessing.py
+++ utils/data_preprocessing.py
@@ -33,8 +33,8 @@ def preprocess_data(df, save_data):
         attack_total = len(df[df['Label'] != "Benign"])
     print("ToTal begnign: " + str(benign_total))
     print("ToTal attack: " + str(attack_total))
-    excluded = ['Destination Port', 'Protocol', 'Timestamp', 'Init_Win_bytes_backward', 'Init_Win_bytes_forward', 'Dst Port', 'Init Fwd Win Byts', 'Init Bwd Win Byts, Packet Length Mean', 'Avg Fwd Segment Size', 'Subflow Fwd Bytes', 'Fwd Packets/s', 'Fwd IAT Total', 'Fwd IAT Max']
+    excluded = ['Destination Port', 'Protocol', 'Timestamp', 'Init_Win_bytes_backward', 'Init_Win_bytes_forward', 'Dst Port', 'Init Fwd Win Byts', 'Init Bwd Win Byts', 'Flow Duration', 'Fwd Packet Length Max', 'Fwd Packet Length Min', 'Bwd Packet Length Max', 'Bwd Packet Length Min', 'Fwd IAT Total', 'Bwd IAT Total', 'Fwd PSH Flags', 'Bwd PSH Flags', 'Fwd URG Flags', 'Bwd URG Flags', 'Fwd Packets/s', 'Bwd Packets/s', 'Average Packet Size', 'Subflow Fwd Packets', 'Avg Fwd Segment Size', 'Avg Bwd Segment Size', 'Subflow Fwd Bytes', 'Subflow Bwd Packets', 'Subflow Bwd Bytes']
     df = df.drop(columns=excluded, errors='ignore')
-    #excluded2 = ['Init_Win_bytes_backward', 'Init_Win_bytes_forward']
+    #excluded2 = ['Init_Win_bytes_backward', 'Init_Win_bytes_forward', Packet Length Mean', 'Avg Fwd Segment Size', 'Subflow Fwd Bytes', 'Fwd Packets/s', 'Fwd IAT Total', 'Fwd IAT Max']
     #df = df.drop(columns=excluded2, errors='ignore')
     df.to_csv(save_data + "web_attacks.csv", index=False)
